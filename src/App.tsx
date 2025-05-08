e63dc03bb55b6c8ce8c16a24dffe4006ed5af583
The <Groundwalkers@outlook.com>
2025-05-08 01:54:43 +0100

    add  editor and preview placeholder functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [editorText, setEditorText] = useState<string>('');
+
+  const handleEditorChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEditorText(event.target.value);
+  };
 
   return (
     <>
-     
+      <div id="main">
+        <h1>Markdown Previewer</h1>
+        <textarea id="editor" placeholder="Type here..." onChange={handleEditorChange}></textarea>
+        <div id="preview">{editorText}</div>
+      </div>
     </>
   )
 }

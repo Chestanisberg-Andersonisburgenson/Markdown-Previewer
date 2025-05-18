53ffd0f5aaf9910efc97d8713c37e96794ce6fbf
The <Groundwalkers@outlook.com>
2025-05-18 03:09:52 +0100

    fix: update base URL in Vite configuration to relative path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  base: 'https://github.com/Chestanisberg-Andersonisburgenson/Markdown-Previewer',
+  base: '/Markdown-Previewer/',
   plugins: [react()],
   build: {
     outDir: 'dist'

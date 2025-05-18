d74ace5291e01f1fd11b972d77df45b4ae63162f
The <Groundwalkers@outlook.com>
2025-05-18 02:47:42 +0100

    feat: update Vite configuration to set base URL and ensure output directory is 'dist'

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: 'https://github.com/Chestanisberg-Andersonisburgenson/Markdown-Previewer',
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
